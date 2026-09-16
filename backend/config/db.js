bec463c7016ab2ab39252395d3dac77318f7edb9
Daumiinos <gaetan.legrand.dev@gmail.com>
2026-09-16 16:03:42 +0200

    fix: filtres, contact, encodage BDD, accents badges

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,5 +9,6 @@ export const pool = new Pool({
   database: process.env.PG_DATABASE || "vite_gourmand",
   user:     process.env.PG_USER     || "postgres",
   password: process.env.PG_PASSWORD || "Paris.123456*",
+  client_encoding: 'UTF8',
   ...(isProduction && { ssl: { rejectUnauthorized: false } })
 });
